300a7f4cb84c5a6a5dc014229f165f95955872d3
Alfonso Casajus <60479980+AlfonsoCasajus@users.noreply.github.com>
2025-02-22 16:13:33 -0300

    add swager

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -13,6 +14,16 @@ async function bootstrap() {
 			}
 		})
 	);
+
+	const config = new DocumentBuilder()
+		.setTitle('Afrus Test')
+		.setDescription('API desarrollada para el test de Afrus')
+		.setVersion('1.0')
+		.addTag('afrus')
+		.build();
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory);
 
 	app.enableCors({
 		origin: process.env.FRONTEND_URL,
